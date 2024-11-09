9c503e7f0547f72586cf9288e5f1c0a16a20a62c
PratikshaLohokare <pratikshalohokare15@gmail.com>
2024-11-09 10:20:47 +0530

    Initial Commit

---
+++ Rk2006project.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (){
+	printf("My name is RK\n");
+	printf("I am from Earth\n");
+	
+	int a,b;
+	a=10;
+	b=15;
+	int sum;
+	sum=a+b;
+	printf("The sum of a and b is %d",sum);
+	
+	return 0;
+}
